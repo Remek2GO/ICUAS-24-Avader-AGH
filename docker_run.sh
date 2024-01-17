bef3a408eea89041faa3541b079eb205121f6311
Kacper Iwicki <69985093+kapi117@users.noreply.github.com>
2024-01-17 23:11:52 +0100

    created package for development and added volumes to docker_run

--- docker_run.sh
+++ docker_run.sh
@@ -2,6 +2,8 @@
 
 XSOCK=/tmp/.X11-unix
 XAUTH=/tmp/.docker.xauth
+SRC_LOCAL=${PWD}/avader_src/
+SRC_DOCKER=/root/sim_ws/src/avader_src/
 touch $XAUTH
 xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
 
@@ -63,6 +65,8 @@ docker run \
   --privileged \
   --volume=$XSOCK:$XSOCK:rw \
   --volume=$XAUTH:$XAUTH:rw \
+  --volume=$SRC_LOCAL:$SRC_DOCKER \
+  --volume=${PWD}/startup/challenge:/root/sim_ws/src/icuas24_competition/startup/challenge \
   --env="XAUTHORITY=${XAUTH}" \
   --env DISPLAY=$DISPLAY \
   --env TERM=xterm-256color \
