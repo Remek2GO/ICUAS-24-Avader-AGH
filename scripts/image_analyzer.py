a52bd00ec151eb7aeb0d48fa51f3d47622a2c1ed
Hubciosz <szolc@agh.edu.pl>
2024-02-14 14:31:59 +0100

    Display image analyzer result

--- scripts/image_analyzer.py
+++ scripts/image_analyzer.py
@@ -24,16 +24,20 @@ class ImageAnalyzer:
         self.plant_beds: Dict[int, PlantBed] = {}
         self.fruit_type: PlantType = None
 
-        self.image_sub = rospy.Subscriber(self.image_topic, ImageForAnalysis, self.image_callback)
-        self.current_fruit_count_pub = rospy.Publisher("/current_fruit_count", Int32, queue_size=10)
-        
+        self.image_sub = rospy.Subscriber(
+            self.image_topic, ImageForAnalysis, self.image_callback
+        )
+        self.current_fruit_count_pub = rospy.Publisher(
+            "/current_fruit_count", Int32, queue_size=10
+        )
+
         self.sub_plants_beds = rospy.Subscriber(
             "/red/plants_beds", String, self.set_fruit_type
         )
-        
+
     def set_fruit_type(self, data: String):
         type = data.data.split(" ")[0]
-        
+
         self.fruit_type = PlantType(type.upper())
 
     def image_callback(self, msg: ImageForAnalysis):
@@ -44,31 +48,40 @@ class ImageAnalyzer:
         if image_for_analysis.img_id == 10:
             I = cv2.imread(image_for_analysis.img_path_color)
             D = cv2.imread(image_for_analysis.img_path_depth)
-            
-            plant_sides, type = detect_fruits.process_frame(I, D)
-                
+
+            plant_sides, type = detect_fruits.process_frame(I, D, True)
+
             if not image_for_analysis.bed_id in self.plant_beds:
                 self.plant_beds[image_for_analysis.bed_id] = PlantBed()
-                
+
             for i, plant_side in enumerate(plant_sides):
-                idx = i if image_for_analysis.bed_side == 0 else len(plant_sides) - i - 1
-                self.plant_beds[image_for_analysis.bed_id].set_plant(idx, image_for_analysis.bed_side, plant_side.fruit_count, plant_side.fruit_position.copy(), plant_side.fruit_type)
-                
+                idx = (
+                    i if image_for_analysis.bed_side == 0 else len(plant_sides) - i - 1
+                )
+                self.plant_beds[image_for_analysis.bed_id].set_plant(
+                    idx,
+                    image_for_analysis.bed_side,
+                    plant_side.fruit_count,
+                    plant_side.fruit_position.copy(),
+                    plant_side.fruit_type,
+                )
+
             # rospy.loginfo(f"Plant bed {image_for_analysis.bed_id} updated")
-            rospy.loginfo(f"Bed #{image_for_analysis.bed_id} side {image_for_analysis.bed_side} found {sum([side.fruit_count for side in plant_sides])} fruits")
+            rospy.loginfo(
+                f"Bed #{image_for_analysis.bed_id} side {image_for_analysis.bed_side} found {sum([side.fruit_count for side in plant_sides])} fruits"
+            )
             rospy.loginfo(f"Current fruit count: {self.get_fruit_count(True)}")
-            
-            
-    def get_fruit_count(self, debug = False) -> int:
+
+    def get_fruit_count(self, debug=False) -> int:
         sums = 0
         for bed_id in self.plant_beds.keys():
             no_fruits = self.plant_beds[bed_id].get_bed_fruit_count(self.fruit_type)
             if debug:
                 rospy.loginfo(f"Bed #{bed_id} total fruit count: {no_fruits}")
             sums += no_fruits
-            
+
         return sums
-    
+
     def run(self):
         rate = rospy.Rate(100)
         while not rospy.is_shutdown():
@@ -82,8 +95,9 @@ class ImageAnalyzer:
                 self.current_fruit_count_pub.publish(current_fruit_count)
             rate.sleep()
 
+
 if __name__ == "__main__":
-    rospy.init_node('image_analyzer', anonymous=True)
-    rospy.loginfo('ImageAnalyzer node started')
+    rospy.init_node("image_analyzer", anonymous=True)
+    rospy.loginfo("ImageAnalyzer node started")
     image_analyzer = ImageAnalyzer()
     image_analyzer.run()
