fix rotation

scripts/photo_logger.py
@@ -204,7 +204,7 @@ class PhotoLogger:
             # rospy.logdebug(f"[Photo Logger] Roll: {odom_data[ROLL_IDX]} "
             #                f"Yaw diff: {yaw_diff}")
             if (
-                odom_data[ROLL_IDX] < ROLL_THRESHOLD
+                np.abs(odom_data[ROLL_IDX]) < ROLL_THRESHOLD
                 and yaw_diff < YAW_THRESHOLD
             ):
                 img_color = bridge.imgmsg_to_cv2(self.current_color_msg, "bgr8")

scripts/utils/A_star.py
@@ -22,10 +22,10 @@ class A_star:
         self.path = []
 
         self.num_chebyshev_between_beds = 15
-        self.num_chebyshev_between_beds_angle = self.num_chebyshev_between_beds + 10
         self.num_chebyshev_back_to_start = 10
         self.num_chebyshev_along_beds_to_start = 10
-        self.num_chebyshev_change_beds = 3
+        self.num_chebyshev_change_beds = 5
+        self.num_times_repeat_point = 5
 
     def add_setpoint(self, number, setpoint: Setpoint):
         self.setpoints.update({number: setpoint})
@@ -181,11 +181,15 @@ class A_star:
 
                     # chebyshev nodes - interpolacja
                     new_points = generate_intermediate_points(
-                        previous_point, current_point, self.num_chebyshev_between_beds
+                        previous_point, current_point, self.num_chebyshev_between_beds, False
                     )
                     new_path += new_points
 
-                    new_path.append(current_point)
+                    # powtorzenie punktu docelowego n razy
+                    for i in range(self.num_times_repeat_point):
+                        new_path.append(current_point)
+
+
                     previous_point = current_point
 
                 else:  # jezeli punkty maja rozne wspolrzedne x - zmiana regalu
@@ -206,7 +210,7 @@ class A_star:
                     new_points = generate_intermediate_points(
                         previous_point,
                         edge_previous_point,
-                        self.num_chebyshev_change_beds,
+                        self.num_chebyshev_change_beds,False
                     )
                     new_path += new_points
 
@@ -237,7 +241,10 @@ class A_star:
                     )
                     new_path += new_points
 
-                    new_path.append(current_point)
+                    # powtorzenie punktu docelowego n razy
+                    for i in range(self.num_times_repeat_point):
+                        new_path.append(current_point)
+
 
                     # Aktualizacja poprzedniego punktu
                     previous_point = current_point
@@ -268,6 +275,10 @@ class A_star:
         )
         new_path += new_points
 
+        previous_end_point = copy.copy(end_point)
+        previous_end_point[2] = 2
+
+        new_path.append(previous_end_point)
         new_path.append(end_point)
 
         # if DEBUG_MODE:
@@ -299,15 +310,20 @@ def chebyshev_nodes(n, a, b):
     return xk_norm
 
 
-def generate_intermediate_points(previous_point, point, n):
+def generate_intermediate_points(previous_point, point, n, change_bed=True):
     a = np.array(previous_point)
     b = np.array(point)
+
+    if np.all(a[:3] == b[:3]):
+        n = n // 2
+
     xk_norm = chebyshev_nodes(n, a, b)
     # xk_norm_angle = chebyshev_nodes(n, a, b)
 
-    mid_point = len(xk_norm) // 2
-    for i in range(mid_point):
-        xk_norm[i][3:] = previous_point[3:]
+    if not change_bed:
+        mid_point = len(xk_norm) // 3
+        for i in range(mid_point):
+            xk_norm[i][3:] = previous_point[3:]
 
     new_points = []
     for i in range(len(xk_norm)):
@@ -325,7 +341,7 @@ def start(AREAS_FROM_DRONE):
     LOCALIZATION = positions.POINTS_OF_INTEREST
     HEURISTIC = positions.matrix_of_distances()
     Astar_fly = A_star(
-        start_location=Setpoint(1.5, 1.5, 1, 0, 0, 0),
+        start_location=Setpoint(1, 1, 1, 0, 0, 0),
         end_location=Setpoint(1, 1, 1, 0, 0, 0),
         heuristic=HEURISTIC,
     )

scripts/utils/positions.py
@@ -63,12 +63,12 @@ def get_distance(p1_indeces: Tuple[int, int], p2_indeces: Tuple[int, int]) -> fl
         # The same side of the beds, so the distance is just the euclidean distance
         return np.sqrt(
             (p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2 + (p1[2] - p2[2]) ** 2
-        )
+        ) + abs(p1[5] - p2[5]) / 2
     elif p1_indeces[0] == 0 or p2_indeces == 0:
         # One of the points is the start position, so the distance is the sum of the
         # euclidean distance and the distance to the start position
         distance = np.sqrt(abs(p2[0] - p1[0]) ** 2 + max(p1[2], p2[2]) ** 2)
-        distance += max(abs(p1[1] - 2), abs(p2[1] - 2))
+        distance += max(abs(p1[1] - 2), abs(p2[1] - 2)) + abs(p1[5] - p2[5]) / 2
         return distance
     else:
         # Both points are on different sides of the beds, so the distance is the sum of
@@ -78,7 +78,7 @@ def get_distance(p1_indeces: Tuple[int, int], p2_indeces: Tuple[int, int]) -> fl
         distance += min(
             abs(p1[1] - 2) + abs(p2[1] - 2), abs(p1[1] - 25) + abs(p2[1] - 25)
         )
-        distance = np.sqrt(distance**2 + (p1[2] - p2[2]) ** 2)
+        distance = np.sqrt(distance**2 + (p1[2] - p2[2]) ** 2) + abs(p1[5] - p2[5]) / 2
         return distance
 
 