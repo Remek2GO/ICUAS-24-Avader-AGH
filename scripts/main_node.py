b665f990064d19babee866631ac530379d90512e
Hubert Szolc <szolc@agh.edu.pl>
2024-05-15 12:54:01 +0200

    Camera image passthrough

--- scripts/main_node.py
+++ scripts/main_node.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python
 """Script to start the main node of the package."""
 
+from cv_bridge import CvBridge
+import cv2
+import numpy as np
 import rospy
-from sensor_msgs.msg import CompressedImage, NavSatFix, Imu, PointCloud2
+from sensor_msgs.msg import CompressedImage, NavSatFix, Image, Imu, PointCloud2
 
 TOPIC_CAMERA = "/camera/color/image_raw/compressed"
+TOPIC_FRUIT_DETECTIONS = "/fruit_detections"
 TOPIC_GPS = "/hawkblue/mavros/global_position/global"
 TOPIC_IMU = "/hawkblue/mavros/imu/data"
 TOPIC_LIDAR = "/velodyne_points"
@@ -13,8 +17,15 @@ TOPIC_LIDAR = "/velodyne_points"
 class MainNode:
     """Class for the main node of the package."""
 
-    def __init__(self):
+    def __init__(self, frequency: float):
         """Initialize the main node."""
+        self._camera_image: np.ndarray = None
+        self._cv_bridge = CvBridge()
+        self._rate = rospy.Rate(frequency)
+
+        # ROS publishers
+        self._pub_camera = rospy.Publisher(TOPIC_FRUIT_DETECTIONS, Image, queue_size=1)
+
         # ROS subscribers
         rospy.Subscriber(TOPIC_CAMERA, CompressedImage, self._clb_camera)
         rospy.Subscriber(TOPIC_GPS, NavSatFix, self._clb_gps)
@@ -23,7 +34,7 @@ class MainNode:
 
     def _clb_camera(self, msg: CompressedImage):
         """Process the camera image."""
-        rospy.loginfo("Received camera image.")
+        self._camera_image = self._cv_bridge.compressed_imgmsg_to_cv2(msg)
 
     def _clb_gps(self, msg: NavSatFix):
         """Process the GPS data."""
@@ -37,8 +48,28 @@ class MainNode:
         """Process the LiDAR data."""
         rospy.loginfo("Received LiDAR data.")
 
+    def get_camera_image(self):
+        """Get the camera image."""
+        if self._camera_image is None:
+            return np.zeros((480, 640, 3), dtype=np.uint8)
+        return self._camera_image
+
+    def publish_fruit_detections(self):
+        """Publish the fruit detections in the image."""
+        camera_image = self.get_camera_image()
+        msg = self._cv_bridge.cv2_to_imgmsg(camera_image, "bgr8")
+        self._pub_camera.publish(msg)
+
+    def run_processing(self):
+        """Run the processing."""
+        rospy.loginfo("Running processing.")
+
+        while not rospy.is_shutdown():
+            self._rate.sleep()
+            self.publish_fruit_detections()
+
 
 if __name__ == "__main__":
     rospy.init_node("main_node", log_level=rospy.INFO)
-    main_node = MainNode()
-    rospy.spin()
+    main_node = MainNode(frequency=50.0)
+    main_node.run_processing()
